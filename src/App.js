cec2ad2dec84402ec2fa329ceb039e810642baa1
nayoonle <nayoonle2@gmail.com>
2021-07-15 08:23:51 -0500

    Added button for linking to form
    linked form for clear-covid-19

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Welcome to my webpage!
       </header>
+      <br />
+      <div style={{ float: 'left' }}>
+      &nbsp; &nbsp; &nbsp;
+      <Button
+          variant="contained"
+          size="large"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdoVBxyjEuYYi5yZ9S_Oh8AOYnu7qlnoWD9qWaRLdcfs-KrIg/viewform"
+          target="_blank"
+        >
+          Registration
+        </Button>
+      </div>
     </div>
   );
 }
